5d7bc747e7b529b5531fcb2b3e69b63c5a8ab7ad
simonalong <simonalongs@outlook.com>
2019-05-23 11:37:04 +0800

    fix: 添加readme.md

--- Ocean/src/main/java/com/simon/ocean/SizeUtil.java
+++ Ocean/src/main/java/com/simon/ocean/SizeUtil.java
@@ -1,27 +1,27 @@
-package com.simon.ocean;
-
-import lombok.experimental.UtilityClass;
-import org.apache.lucene.util.RamUsageEstimator;
-
-/**
- * @author zhouzhenyong
- * @since 2019/2/22 下午3:13
- */
-@UtilityClass
-public class SizeUtil {
-
-    public long sizeOf(Object object){
-        return RamUsageEstimator.sizeOf(object);
-    }
-
-    /**
-     * 数据尺寸直接显示尺寸的大小，Byte, KB, MB, GB
-     */
-    public String strSizeOf(Object object){
-        return RamUsageEstimator.humanSizeOf(object);
-    }
-
-    public String strSizeOf(Integer num, Object object){
-        return RamUsageEstimator.humanReadableUnits(num * sizeOf(object));
-    }
-}
+//package com.simon.ocean;
+//
+//import lombok.experimental.UtilityClass;
+//import org.apache.lucene.util.RamUsageEstimator;
+//
+///**
+// * @author zhouzhenyong
+// * @since 2019/2/22 下午3:13
+// */
+//@UtilityClass
+//public class SizeUtil {
+//
+//    public long sizeOf(Object object){
+//        return RamUsageEstimator.sizeOf(object);
+//    }
+//
+//    /**
+//     * 数据尺寸直接显示尺寸的大小，Byte, KB, MB, GB
+//     */
+//    public String strSizeOf(Object object){
+//        return RamUsageEstimator.humanReadableUnits(object);
+//    }
+//
+//    public String strSizeOf(Integer num, Object object){
+//        return RamUsageEstimator.humanReadableUnits(num * sizeOf(object));
+//    }
+//}
